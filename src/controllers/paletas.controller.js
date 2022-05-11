7cc4af29747ec3efa11f821c144fd7a66bbed33a
LucasVedoi <89050672+LucasVedoi@users.noreply.github.com>
2022-05-10 21:02:29 -0300

    Add files via upload

---
+++ src/controllers/paletas.controller.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const paletasService = require('../services/paletas.service');
+
+const findAllPaletasController = async (req, res) => {
+  const allPaletas = await paletasService.findPaletasService();
+  if (allPaletas.length == 0) {
+    return res
+      .status(404)
+      .send({ message: 'Não existe nenhuma paleta cadastrada!' });
+  }
+  res.send(allPaletas);
+};
+
+const findPaletaByIdController = async (req, res) => {
+  const idParam = req.params.id;
+  const chosenPaleta = await paletasService.findPaletaByIdService(idParam);
+  if (!chosenPaleta) {
+    return res.status(404).send({ message: 'Paleta não encontrada!' });
+  }
+  res.send(chosenPaleta);
+};
+
+const createPaletaController = async (req, res) => {
+  const paleta = req.body;
+  const newPaleta = await paletasService.createPaletaService(paleta);
+  res.status(201).send(newPaleta);
+};
+
+const updatePaletaController = async (req, res) => {
+  const idParam = req.params.id;
+  const editPaleta = req.body;
+  const updatedPaleta = await paletasService.updatePaletaService(
+    idParam,
+    editPaleta,
+  );
+  res.send(updatedPaleta);
+};
+
+const deletePaletaController = async (req, res) => {
+  const idParam = req.params.id;
+  await paletasService.deletePaletaService(idParam);
+  res.send({ message: 'Paleta deletada com sucesso!' });
+};
+
+module.exports = {
+  findAllPaletasController,
+  findPaletaByIdController,
+  createPaletaController,
+  updatePaletaController,
+  deletePaletaController,
+};
